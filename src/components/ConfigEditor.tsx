a38426de914df98d9df47bf69fcbb9aa84da45dc
Sarah Zinger <sarah.zinger@grafana.com>
2025-04-13 11:47:25 -0400

    Config Page Changes

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -9,61 +9,80 @@ export function ConfigEditor(props: Props) {
   const { onOptionsChange, options } = props;
   const { jsonData, secureJsonFields, secureJsonData } = options;
 
-  const onPathChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const onDomainChange = (event: ChangeEvent<HTMLInputElement>) => {
     onOptionsChange({
       ...options,
       jsonData: {
         ...jsonData,
-        path: event.target.value,
+        domain: event.target.value,
+      },
+    });
+  };
+
+  const onResourceChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onOptionsChange({
+      ...options,
+      jsonData: {
+        ...jsonData,
+        resource: event.target.value,
       },
     });
   };
 
   // Secure field (only sent to the backend)
-  const onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const onAppTokenChange = (event: ChangeEvent<HTMLInputElement>) => {
     onOptionsChange({
       ...options,
       secureJsonData: {
-        apiKey: event.target.value,
+        appToken: event.target.value,
       },
     });
   };
 
-  const onResetAPIKey = () => {
+  const onResetAppToken = () => {
     onOptionsChange({
       ...options,
       secureJsonFields: {
         ...options.secureJsonFields,
-        apiKey: false,
+        appToken: false,
       },
       secureJsonData: {
         ...options.secureJsonData,
-        apiKey: '',
+        appToken: '',
       },
     });
   };
 
   return (
     <>
-      <InlineField label="Path" labelWidth={14} interactive tooltip={'Json field returned to frontend'}>
+      <InlineField label="Domain" labelWidth={14} interactive tooltip={'Json field returned to frontend'}>
         <Input
-          id="config-editor-path"
-          onChange={onPathChange}
-          value={jsonData.path}
-          placeholder="Enter the path, e.g. /api/v1"
+          id="config-editor-domain"
+          onChange={onDomainChange}
+          value={jsonData.domain}
+          placeholder="Enter the domain, e.g. http://host.docker.internal:8080 or https://data.seattle.gov"
           width={40}
         />
       </InlineField>
-      <InlineField label="API Key" labelWidth={14} interactive tooltip={'Secure json field (backend only)'}>
+      <InlineField label="Resource" labelWidth={14} interactive tooltip={'Json field returned to frontend'}>
+        <Input
+          id="config-editor-resource"
+          onChange={onResourceChange}
+          value={jsonData.resource}
+          placeholder="Enter the resource, e.g. egc4-d24i"
+          width={40}
+        />
+      </InlineField>
+      <InlineField label="App Token" labelWidth={14} interactive tooltip={'Secure json field (backend only)'}>
         <SecretInput
           required
-          id="config-editor-api-key"
-          isConfigured={secureJsonFields.apiKey}
-          value={secureJsonData?.apiKey}
-          placeholder="Enter your API key"
+          id="config-editor-app-token"
+          isConfigured={secureJsonFields.appToken}
+          value={secureJsonData?.appToken}
+          placeholder="Enter your App Token"
           width={40}
-          onReset={onResetAPIKey}
-          onChange={onAPIKeyChange}
+          onReset={onResetAppToken}
+          onChange={onAppTokenChange}
         />
       </InlineField>
     </>
--- src/types.ts
+++ src/types.ts
@@ -23,12 +23,13 @@ export interface DataSourceResponse {
  * These are options configured for each DataSource instance
  */
 export interface MyDataSourceOptions extends DataSourceJsonData {
-  path?: string;
+  domain: string;
+  resource: string;
 }
 
 /**
  * Value that is used in the backend, but never sent over HTTP to the frontend
  */
 export interface MySecureJsonData {
-  apiKey?: string;
+  appToken?: string;
 }
